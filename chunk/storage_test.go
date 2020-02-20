1423e6d12d5cbf8b2eaaa862ac485aff53387592
Felix Bünemann <felix.buenemann@gmail.com>
2020-02-20 01:58:13 +0100

    Tweak storage tests and add parallel benchmark

--- chunk/storage_test.go
+++ chunk/storage_test.go
@@ -17,13 +17,23 @@ func TestStore(t *testing.T) {
 	n := maxChunks * 2
 	t.Run("Mem", func(t *testing.T) {
 		useMmap := false
-		testStore(chunkSize, maxChunks, useMmap, n, func(format string, args ...interface{}) {
+		storage, err := newStorage(chunkSize, maxChunks, useMmap)
+		if nil != err {
+			t.Fatal(err)
+		}
+		defer storage.Close()
+		testStore(storage, chunkSize, n, func(format string, args ...interface{}) {
 			t.Fatalf(format, args...)
 		})
 	})
 	t.Run("Disk", func(t *testing.T) {
 		useMmap := true
-		testStore(chunkSize, maxChunks, useMmap, n, func(format string, args ...interface{}) {
+		storage, err := newStorage(chunkSize, maxChunks, useMmap)
+		if nil != err {
+			t.Fatal(err)
+		}
+		defer storage.Close()
+		testStore(storage, chunkSize, n, func(format string, args ...interface{}) {
 			t.Fatalf(format, args...)
 		})
 	})
@@ -35,13 +45,29 @@ func TestLoad(t *testing.T) {
 	n := maxChunks
 	t.Run("Mem", func(t *testing.T) {
 		useMmap := false
-		testLoad(chunkSize, maxChunks, useMmap, n, func(format string, args ...interface{}) {
+		storage, err := newStorage(chunkSize, maxChunks, useMmap)
+		if nil != err {
+			t.Fatal(err)
+		}
+		defer storage.Close()
+		testStore(storage, chunkSize, n, func(format string, args ...interface{}) {
+			t.Fatalf(format, args...)
+		})
+		testLoad(storage, chunkSize, n, func(format string, args ...interface{}) {
 			t.Fatalf(format, args...)
 		})
 	})
 	t.Run("Disk", func(t *testing.T) {
 		useMmap := true
-		testLoad(chunkSize, maxChunks, useMmap, n, func(format string, args ...interface{}) {
+		storage, err := newStorage(chunkSize, maxChunks, useMmap)
+		if nil != err {
+			t.Fatal(err)
+		}
+		defer storage.Close()
+		testStore(storage, chunkSize, n, func(format string, args ...interface{}) {
+			t.Fatalf(format, args...)
+		})
+		testLoad(storage, chunkSize, n, func(format string, args ...interface{}) {
 			t.Fatalf(format, args...)
 		})
 	})
@@ -49,34 +75,67 @@ func TestLoad(t *testing.T) {
 
 func BenchmarkStore(b *testing.B) {
 	chunkSize := int64(10 << 20) // 10M
-	maxChunks := 32
+	maxChunks := 10
 	b.Run("Mem", func(b *testing.B) {
 		useMmap := false
-		testStore(chunkSize, maxChunks, useMmap, b.N, func(format string, args ...interface{}) {
+		storage, err := newStorage(chunkSize, maxChunks, useMmap)
+		if nil != err {
+			b.Fatal(err)
+		}
+		defer storage.Close()
+		testStore(storage, chunkSize, b.N, func(format string, args ...interface{}) {
 			b.Fatalf(format, args...)
 		})
 	})
 	b.Run("Disk", func(b *testing.B) {
 		useMmap := true
-		testStore(chunkSize, maxChunks, useMmap, b.N, func(format string, args ...interface{}) {
+		storage, err := newStorage(chunkSize, maxChunks, useMmap)
+		if nil != err {
+			b.Fatal(err)
+		}
+		defer storage.Close()
+		testStore(storage, chunkSize, b.N, func(format string, args ...interface{}) {
 			b.Fatalf(format, args...)
 		})
 	})
 }
 
-func testStore(chunkSize int64, maxChunks int, useMmap bool, n int, errback func(string, ...interface{})) {
-	// Silence logger
-	logLevel := Log.Level()
-	if logLevel != loggo.Fatal {
-		Log.SetLevel(loggo.Fatal)
-		defer Log.SetLevel(logLevel)
-	}
-	storage, err := newStorage(chunkSize, maxChunks, useMmap)
+func BenchmarkParallelStore(b *testing.B) {
+	chunkSize := int64(10 << 20) // 10M
+	maxChunks := 10
+	useMmap := false
+	memStorage, err := newStorage(chunkSize, maxChunks, useMmap)
 	if nil != err {
-		errback("Failed to init storage: %v", err)
-		return
+		b.Fatal(err)
 	}
-	defer storage.Close()
+	defer memStorage.Close()
+	b.Run("Mem", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				testStore(memStorage, chunkSize, maxChunks, func(format string, args ...interface{}) {
+					b.Fatalf(format, args...)
+				})
+			}
+		})
+	})
+	useMmap = true
+	diskStorage, err := newStorage(chunkSize, maxChunks, useMmap)
+	if nil != err {
+		b.Fatal(err)
+	}
+	defer diskStorage.Close()
+	b.Run("Disk", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				testStore(diskStorage, chunkSize, maxChunks, func(format string, args ...interface{}) {
+					b.Fatalf(format, args...)
+				})
+			}
+		})
+	})
+}
+
+func testStore(storage *Storage, chunkSize int64, n int, errback func(string, ...interface{})) {
 	data := make([]byte, chunkSize)
 	for i := 0; i < n; i++ {
 		// Populate buffer with binary encoded value of i
@@ -94,34 +153,8 @@ func testStore(chunkSize int64, maxChunks int, useMmap bool, n int, errback func
 	}
 }
 
-func testLoad(chunkSize int64, maxChunks int, useMmap bool, n int, errback func(string, ...interface{})) {
-	// Silence logger
-	logLevel := Log.Level()
-	if logLevel != loggo.Fatal {
-		Log.SetLevel(loggo.Fatal)
-		defer Log.SetLevel(logLevel)
-	}
-	storage, err := newStorage(chunkSize, maxChunks, useMmap)
-	if nil != err {
-		errback("Failed to init storage: %v", err)
-		return
-	}
-	defer storage.Close()
+func testLoad(storage *Storage, chunkSize int64, n int, errback func(string, ...interface{})) {
 	data := make([]byte, chunkSize)
-	for i := 0; i < n; i++ {
-		// Populate buffer with binary encoded value of i
-		binary.LittleEndian.PutUint64(data, uint64(i))
-		chunk, err := storage.Store(strconv.Itoa(i), data)
-		if nil != err {
-			errback("Failed to store buffer %v: %v", i, err)
-			return
-		}
-		j := int(binary.LittleEndian.Uint64(chunk.Bytes))
-		chunk.Close()
-		if j != i {
-			errback("Buffer mismatch – Expected %v got %v", i, j)
-		}
-	}
 	for i := 0; i < n; i++ {
 		// Populate buffer with binary encoded value of i
 		binary.LittleEndian.PutUint64(data, uint64(i))
@@ -129,12 +162,11 @@ func testLoad(chunkSize int64, maxChunks int, useMmap bool, n int, errback func(
 		if nil == chunk {
 			errback("Chunk %v not found", i)
 			return
-		} else {
-			j := int(binary.LittleEndian.Uint64(chunk.Bytes))
-			chunk.Close()
-			if j != i {
-				errback("Buffer mismatch – Expected %v got %v", i, j)
-			}
+		}
+		j := int(binary.LittleEndian.Uint64(chunk.Bytes))
+		chunk.Close()
+		if j != i {
+			errback("Buffer mismatch – Expected %v got %v", i, j)
 		}
 	}
 }
@@ -142,6 +174,9 @@ func testLoad(chunkSize int64, maxChunks int, useMmap bool, n int, errback func(
 func newStorage(chunkSize int64, maxChunks int, useMmap bool) (*Storage, error) {
 	var err error
 	var chunkFile *os.File
+	// Silence logger
+	Log.SetLevel(loggo.Fatal)
+	Log.Infof("Initializing storage: chunkSize=%v, maxChunks=%v, useMmap=%v", chunkSize, maxChunks, useMmap)
 	if useMmap {
 		chunkFile, err = ioutil.TempFile("", "plexdrive-chunks-*.dat")
 		if err != nil {
